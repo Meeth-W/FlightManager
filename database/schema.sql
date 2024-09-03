cc84bac6abd970c9fc0f330c15f95186697cb9ff
Meeth <meeth2111@gmail.com>
2024-09-03 08:41:51 +0530

    🚧 Changed Schema

--- database/schema.sql
+++ database/schema.sql
@@ -1,26 +1,25 @@
-CREATE TABLE IF NOT EXISTS `user` (
-    `user_id` int PRIMARY KEY,
-    `user_name` varchar(20) NOT NULL,
-    `user_password` varchar(20) NOT NULL,
-    `user_permission_level` varchar(10) NOT NULL DEFAULT `base_user`,
-    `user_email` varchar(100) NOT NULL,
-    `user_address` varchar(100) NOT NULL,
-    `user_dateofbirth` DATE NOT NULL,
-    `user_age` INT NOT NULL,
+CREATE TABLE IF NOT EXISTS "user" (
+    "user_id" INTEGER PRIMARY KEY,
+    "user_name" TEXT NOT NULL,
+    "user_password" TEXT NOT NULL,
+    "user_permission_level" TEXT NOT NULL DEFAULT 'base_user',
+    "user_email" TEXT NOT NULL,
+    "user_address" TEXT NOT NULL,
+    "user_dateofbirth" DATE NOT NULL,
+    "user_age" INTEGER NOT NULL
+);
 
-    CHECK (`user_age` >= 18)
-)
-
-CREATE TABLE IF NOT EXISTS `flights` (
-    `id` INT PRIMARY KEY,
-    `flight_number` VARCHAR(10) NOT NULL,
-    `flight_name` VARCHAR(20) NOT NULL,
-    `airline` VARCHAR(10) NOT NULL,
-    `departure_airport` VARCHAR(20) NOT NULL,
-    `arrival_airport` VARCHAR(20) NOT NULL,
-    `flight_date` DATE NOT NULL,
-    `flight_time` INT NOT NULL,
-    `flight_duration` INT NOT NULL,
-    `flight_status` VARCHAR(10) NOT NULL DEFAULT 'on_time',
-    `available_seats` INT NOT NULL,
-)
+CREATE TABLE IF NOT EXISTS "flights" (
+    "id" INTEGER PRIMARY KEY,
+    "flight_number" TEXT NOT NULL,
+    "flight_name" TEXT NOT NULL,
+    "airline" TEXT NOT NULL,
+    "departure_airport" TEXT NOT NULL,
+    "arrival_airport" TEXT NOT NULL,
+    "flight_date" DATE NOT NULL,
+    "flight_time" INTEGER NOT NULL,
+    "flight_duration" INTEGER NOT NULL,
+    "flight_status" TEXT NOT NULL DEFAULT 'on_time',
+    "available_seats" INTEGER NOT NULL,
+    "price" INTEGER NOT NULL
+);
